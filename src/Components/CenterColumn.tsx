36fded9f1fc5703424afc0a6eab3000155e3c49a
marubontan <marubontan@gmail.com>
2020-03-26 19:12:57 -0300

    add files

--- src/Components/CenterColumn.tsx
+++ src/Components/CenterColumn.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import '../App.css'
 
 const centerColumn = () => {
-    return <div className="centerColDesign"><p>Center Column</p></div>;
+    return <div className="card postSize">
+        <div className="card-body horizontalFlex">
+            <img className="floatLeft" src="https://img.icons8.com/color/48/000000/user-male.png" />
+            <form className="verticalFlex" action="/" method="post">
+                <input type="text" name="postContent" />
+                <button className="horizontalFlexEnd">post</button>
+            </form>
+        </div>
+    </div>
 }
 
 export default centerColumn;
